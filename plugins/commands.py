239045f57103e0ab063c9a29d9cf4eccef3449bf
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-19 20:34:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,1 +1,36 @@
 from pyrogram import Client, filters
+
+START_TEXT = """
+**Hello {} 😌
+I am telegram developers bot.**
+
+>> `I have more features for developers`
+
+Made by @FayasNoushad
+"""
+
+HELP_TEXT = """
+**Hey, Follow these steps:**
+
+I am a developer helper bot. Use the commands below for more.
+
+**Available Commands**
+
+/start - Checking Bot Online
+/help - For more help
+/about - For more about me
+
+>> **Please join my [updates channel](https://telegram.me/FayasNoushad) for more bots and updates and Please contact [Fayas](https://telegram.me/TheFayas) for reporting, bugs, requests, and suggestions.**
+
+Made by @FayasNoushad
+"""
+
+ABOUT_TEXT = """
+➠ **Bot :** ``Developers Bot` 🤖
+➠ **Creator :** [Fayas](https://telegram.me/TheFayas)
+➠ **Channel :** [Fayas Noushad](https://telegram.me/FayasNoushad)
+➠ **Credits :** `Everyone in this journey`
+➠ **Language :** [Python3](https://python.org)
+➠ **Library :** [Pyrogram](https://pyrogram.org)
+➠ **Server :** [Heroku](https://heroku.com)
+"""
