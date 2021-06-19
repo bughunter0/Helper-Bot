bc369eda7e6d582e09e5c00e591bd7f23b12f2e2
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-19 20:44:13 +0530

    update json.py

--- plugins/modules/json.py
+++ plugins/modules/json.py
@@ -1,2 +1,12 @@
 from pyrogram import Client as FayasNoushad
 from pyrogram import filters
+
+@FayasNoushad.on_message((filters.group | filters.private) & filters.command(["json"]))
+async def group(bot, update):
+    json = update.reply_to_message
+    with BytesIO(str.encode(str(json))) as json_file:
+        json_file.name = "json.text"
+        await json.reply_document(
+            document=json_file,
+            reply_markup=JSON_BUTTON
+        )
