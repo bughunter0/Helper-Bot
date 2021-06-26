9be26d24d79c0d26b5be191abe09ddc08874b5e7
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-26 12:23:36 +0530

    Update start.py

--- plugins/commands/start.py
+++ plugins/commands/start.py
@@ -1,5 +1,10 @@
 from pyrogram import Client, filters
 from ..commands import *
 
-@Client.on_message(filters.private & filters.command(["start"]) & filters.user(AUTH_USERS) if AUTH_USERS else None)
+@Client.on_message(filters.private & filters.command(["start", "help" "about"]) & filters.user(AUTH_USERS) if AUTH_USERS else None)
 async def start_command(bot, update):
+    text = update.text
+    if text == "/start":
+        await start(bot, update)
+    else:
+        text = text.split(" ", -1)
