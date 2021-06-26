8f57631a2b314e7abf9aa01eed975c7f358efb19
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-26 11:52:33 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -2,65 +2,6 @@ from pyrogram import Client as FayasNoushad
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-START_TEXT = """
-**Hello {} 😌
-I am telegram developers bot.**
-
->> `I have more features for developers`
-
-Made by @FayasNoushad
-"""
-
-HELP_TEXT = """
-**Hey, Follow these steps:**
-
-I am a developer helper bot. Use the commands below for more.
-
-**Available Commands**
-
-/start - Checking Bot Online
-/help - For more help
-/about - For more about me
-
->> **Please join my [updates channel](https://telegram.me/FayasNoushad) for more bots and updates and Please contact [Fayas](https://telegram.me/TheFayas) for reporting, bugs, requests, and suggestions.**
-
-Made by @FayasNoushad
-"""
-
-ABOUT_TEXT = """
-➠ **Bot :** ``Developers Bot` 🤖
-➠ **Creator :** [Fayas](https://telegram.me/TheFayas)
-➠ **Channel :** [Fayas Noushad](https://telegram.me/FayasNoushad)
-➠ **Credits :** `Everyone in this journey`
-➠ **Language :** [Python3](https://python.org)
-➠ **Library :** [Pyrogram](https://pyrogram.org)
-➠ **Server :** [Heroku](https://heroku.com)
-"""
-
-START_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('⚙ Help', callback_data='help'),
-        InlineKeyboardButton('About 🔰', callback_data='about'),
-        InlineKeyboardButton('Close ✖️', callback_data='close')
-        ]]
-    )
-
-HELP_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('🏘 Home', callback_data='home'),
-        InlineKeyboardButton('About 🔰', callback_data='about'),
-        InlineKeyboardButton('Close ✖️', callback_data='close')
-        ]]
-    )
-
-ABOUT_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('🏘 Home', callback_data='home'),
-        InlineKeyboardButton('Help ⚙', callback_data='help'),
-        InlineKeyboardButton('Close ✖️', callback_data='close')
-        ]]
-    )
-
 @FayasNoushad.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
     await update.reply_text(
