e3beaa0b3cd9739cc51cfa46018d0b17952381df
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-26 11:49:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import os
 import pyrogram
 from pyrogram import Client, filters
-from config import Config
+from config import *
 
 plugins = dict(root="plugins")
 
 FayasNoushad = Client(
     "Developers-Bot",
-    bot_token = Config.BOT_TOKEN,
-    api_id = Config.API_ID,
-    api_hash = Config.API_HASH,
+    bot_token = BOT_TOKEN,
+    api_id = API_ID,
+    api_hash = API_HASH,
     plugins = plugins
 )
 
