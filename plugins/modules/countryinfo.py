bcc3d738c5d08f9abf791e6ca6d96a2ab1f3db92
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-26 17:24:04 +0530

    Update countryinfo.py

--- plugins/modules/countryinfo.py
+++ plugins/modules/countryinfo.py
@@ -4,7 +4,11 @@ from countryinfo import CountryInfo
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-@Client.on_message(filters.private & filters.commands(["country_info"]) & filters.user(AUTH_USERS) if AUTH_USERS else None)
+@Client.on_message(
+    filters.private &
+    filters.commands(["country_info", "country_information"]) &
+    filters.user(AUTH_USERS) if AUTH_USERS else None
+)
 async def countryinfo(bot, update):
     country = update.text.split(" ", -1)
     country = CountryInfo(update.text)
