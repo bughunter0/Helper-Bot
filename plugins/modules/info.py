ff7fafb469b4c14a6c569656ec0ab1e74614ba4d
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-30 18:33:36 +0530

    Update info.py

--- plugins/modules/info.py
+++ plugins/modules/info.py
@@ -2,7 +2,7 @@
 
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
+from config import *
 
 @Client.on_message((filters.private | filters.group) & filters.command(["info", "information"]))
 async def info(bot, update):
