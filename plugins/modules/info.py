584dd92e3c64b7dc519107c448c40bd3405c813b
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-30 14:00:49 +0530

    Update info.py

--- plugins/modules/info.py
+++ plugins/modules/info.py
@@ -5,3 +5,24 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message((filters.private | filters.group) & filters.command(["info", "information"]))
 async def info(bot, update):
+
+
+def user_info(user):
+    text = "--**User Details:**--\n"
+    text += f"\n**First Name:** `{user.first_name}`"
+    text += f"\n**Last Name:** `{user.last_name},`" if user.last_name else ""
+    text += f"\n**User Id:** `{user.id}`"
+    text += f"\n**Username:** @{user.username}" if user.username else ""
+    text += f"\n**User Link:** {user.mention}" if user.username else ""
+    text += f"\n**DC ID:** `{user.dc_id}`" if user.dc_id else ""
+    text += f"\n**Is Deleted:** True" if user.is_deleted else ""
+    text += f"\n**Is Bot:** True" if user.is_bot else ""
+    text += f"\n**Is Verified:** True" if user.is_verified else ""
+    text += f"\n**Is Restricted:** True" if user.is_verified else ""
+    text += f"\n**Is Scam:** True" if user.is_scam else ""
+    text += f"\n**Is Fake:** True" if user.is_fake else ""
+    text += f"\n**Is Support:** True" if user.is_support else ""
+    text += f"\n**Language Code:** {user.language_code}" if user.language_code else ""
+    text += f"\n**Status:** {user.status}" if user.status else ""
+    text += f"\n\nMade by @FayasNoushad"
+    return text
