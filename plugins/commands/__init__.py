2fa3113b991a689a13f54a732bbc341eb02c9e40
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-09-05 16:26:17 +0530

    Update __init__.py

--- plugins/commands/__init__.py
+++ plugins/commands/__init__.py
@@ -4,26 +4,24 @@ from config import *
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-async def start(bot, update):
-    await update.reply_text(
-        text=START_TEXT.format(update.from_user.mention),
-        disable_web_page_preview=True,
-        quote=True,
-	reply_markup=START_BUTTONS
-    )
+async def start(bot, update, cb=False):
+    text=START_TEXT.format(update.from_user.mention)
+    if cb:
+        await update.message.edit_text(text=text, disable_web_page_preview=True, reply_markup=START_BUTTONS)
+    else:
+        await update.reply_text(text=text, disable_web_page_preview=True, quote=True, reply_markup=START_BUTTONS)
 
-async def help(bot, update):
-    await update.reply_text(
-        text=HELP_TEXT,
-      	disable_web_page_preview=True,
-        quote=True,
-	reply_markup=HELP_BUTTONS
-    )
 
-async def about(bot, update):
-    await update.reply_text(
-        text=ABOUT_TEXT,
-        disable_web_page_preview=True,
-        quote=True,
-	reply_markup=ABOUT_BUTTONS
-    )
+async def help(bot, update, cb=False):
+    if cb:
+        await update.message.edit_text(text=HELP_TEXT, disable_web_page_preview=True, reply_markup=HELP_BUTTONS)
+    else:
+        await update.reply_text(text=HELP_TEXT, disable_web_page_preview=True, quote=True, reply_markup=HELP_BUTTONS)
+
+
+async def about(bot, update, cb=False):
+    text = ABOUT_TEXT.format((await bot.get_me()).username)
+    if cb:
+        await update.message.edit_text(text=text, disable_web_page_preview=True, reply_markup=ABOUT_BUTTONS)
+    else:
+        await update.reply_text(text=text, disable_web_page_preview=True, quote=True, reply_markup=ABOUT_BUTTONS)
