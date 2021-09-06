5aa8081e50a2d99f4a8dd778f8a533ca381a036e
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-09-06 18:19:07 +0530

    Update __init__.py

--- plugins/modules/__init__.py
+++ plugins/modules/__init__.py
@@ -3,3 +3,12 @@
 # Copyright permission under MIT License
 # All rights reserved by FayasNoushad
 # License -> https://github.com/TelegramHelpBot/Helper-Bot/blob/main/LICENSE
+
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+MODULES = InlineKeyboardMarkup(
+    [
+        []
+    ]
+)
