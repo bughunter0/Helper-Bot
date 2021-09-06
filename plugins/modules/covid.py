c306f4da9a91c1557f9e40ed49ddb3b1f545f2e0
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-09-06 21:19:28 +0530

    Update covid.py

--- plugins/modules/covid.py
+++ plugins/modules/covid.py
@@ -14,10 +14,7 @@ Send /covid with country name like `/covid India`"""
 
 BUTTONS = []
 
-@Client.on_message(
-    filters.command(["corona", "covid", "covid_info", "corona_info"]) &
-    filters.user(BOT_OWNER) if PRIVATE else None
-)
+
 async def covid_info(bot, update):
     try:
         country = update.text.split(" ", 1)[1]
