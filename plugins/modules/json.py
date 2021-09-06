6e050eeebdef20e12b53f0931210d1022e65be70
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-09-06 21:17:35 +0530

    Update json.py

--- plugins/modules/json.py
+++ plugins/modules/json.py
@@ -13,17 +13,6 @@ Send /json command with reply a message."""
 BUTTONS = []
 
 
-@Client.on_message(
-    filters.command(
-        [
-            "json",
-            "update_json",
-            "pyro_json",
-            "pyrogram_json"
-        ]
-    ) &
-    filters.user(BOT_OWNER) if PRIVATE else None
-)
 async def group(bot, update):
     json = update.reply_to_message
     with BytesIO(str.encode(str(json))) as json_file:
