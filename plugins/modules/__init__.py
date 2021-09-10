630006237f3867772819e23290262878fbe3333c
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-09-10 15:27:40 +0530

    Update __init__.py

--- plugins/modules/__init__.py
+++ plugins/modules/__init__.py
@@ -47,7 +47,7 @@ MODULES = {
 
 
 @Client.on_message(filters.command(["modules"]), group=1)
-async def modules_help(bot, update, cb=False):
+async def modules_help(update, cb=False):
     if cb and update.data.startswith("module+"):
         await modules_cb(bot, update)
         return
