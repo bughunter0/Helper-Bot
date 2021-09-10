bb655aa784e3a4997d364a6af14c843ebf188d2d
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-09-10 14:56:44 +0530

    Added youtube thumbnail download module

---
+++ plugins/modules/ytthumb.py
@@ -0,0 +1,39 @@
+import ytthumb
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+@Client.on_message(
+    filters.command(["ytthumb", "thumbnail"]),
+    group=1
+)
+async def youtube_thumbnail(bot, update):
+    reply_markup = InlineKeyboardMarkup(
+        [[InlineKeyboardButton('Join Updates Channel', url='https://telegram.me/FayasNoushad')]]
+    )
+    if len(update.text.split()) == 1 or len(update.text.split()) >= 4:
+        await update.reply_text(
+            "Send command with youtube video link with quality ( optional )",
+            quote=True,
+            reply_markup=reply_markup
+        )
+    else:
+        length = len(update.text.split())
+        video = update.text.split()[1]
+        if length == 2:
+            thumbnail = ytthumb.thumbnail(video)
+        else:
+            quality = update.text.split()[2]
+            thumbnail = ytthumb.thumbnail(video, quality)
+        try:
+            await update.reply_photo(
+                thumbnail,
+                quote=True,
+                reply_markup=reply_markup
+            )
+        except Exception as error:
+            await update.reply_text(
+                text=error,
+                reply_markup=reply_markup
+            )
+            
