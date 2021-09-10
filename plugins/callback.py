09e8a96d27b9ec2a81bac71240d4cb61fc972123
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-09-10 16:38:47 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -19,5 +19,7 @@ async def cb_handler(bot: Client, update: CallbackQuery):
         await about(bot, update, cb=True)
     elif update.data == "close":
         await update.message.delete()
-    elif update.data.startswith("module"):
+    elif update.data.startswith("module+"):
+        await modules_cb(update)
+    elif update.data.startswith("modules"):
         await modules_cb(update, cb=True)
